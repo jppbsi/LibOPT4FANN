560e262a205d5c3354dd78d55dc5b5aa02a3c56c
Joao Papa <papa@Joaos-iMac.local>
2017-11-14 15:12:38 -0200

    Added initial files.

---
+++ include/dev.h
@@ -0,0 +1,37 @@
+#ifndef _DEV_H_
+#define _DEV_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* LibOPF */
+#include "OPF.h"
+
+/* LibDEEP */
+#include "deep.h"
+
+/* LibOPT */
+#include "opt.h"
+#include "pso.h"
+#include "ba.h"
+#include "fpa.h"
+#include "fa.h"
+#include "cs.h"
+#include "gp.h"
+#include "bha.h"
+#include "hs.h"
+
+#include "_opf_.h"
+#include "_dbn_.h"
+#include "_dbm_.h"
+#include "_epnn_.h"
+#include "_featureselection_.h"
+#include "_rbm_.h"
+#include "_regression_.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
